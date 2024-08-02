6709b7937308881b3af7c7f64d208d45ac62f87a
gunil <this_is_one@naver.com>
2024-08-02 12:33:57 +0900

    echo

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.emit("message", "hello");
+
+  socket.on("chat message", (_msg) => {
+    var msg = "" + msg;
+    console.log("message: " + msg);
+    socket.emit("message", "echo " + msg);
   });
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
